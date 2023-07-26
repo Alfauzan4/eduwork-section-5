541d66a929228cb51d5b96b67f229325f73a9e25
Alfauzan4 <alfauzansofiafat@gmail.com>
2023-07-26 17:16:28 +0700

    menambahkan file

---
+++ cypress/e2e/3-tugas-eduwork/section-5-assertions.cy.js
@@ -0,0 +1,17 @@
+describe('Test', () => {
+    it('clicking "type" shows the right headings', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        //Should be on a new URL which includes '/commands/actions'
+        cy.url().should('include', '.io')
+
+        //Get an input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+        .type('alfauzan@gmail.com')
+        .should('have.value', 'alfauzan@gmail.com')
+    })
+})
